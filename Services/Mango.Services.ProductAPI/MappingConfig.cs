c9fe61c17e463f1278ca79e0689adbb213a8616b
Aman Dinesh <xarmaaaruti@gmail.com>
2022-10-18 14:07:12 +0530

    Add project files.

---
+++ Services/Mango.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI
+{
+    //public class MappingConfig
+    //{
+    //    public static MapperConfiguration RegisterMaps()
+    //    {
+    //        var mappingConfig = new MapperConfiguration(config =>
+    //        {
+    //            config.CreateMap<ProductDto, Product>();
+    //            config.CreateMap<ProductDto, Product>();
+    //        });
+
+    //        return mappingConfig;
+    //    }
+    //}
+
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<ProductDto, Product>();
+            CreateMap<Product, ProductDto>();
+        }
+    }
+}
